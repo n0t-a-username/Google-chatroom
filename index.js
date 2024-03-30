0eca8ad686f7b96419f6572c2a09ca299981266c
n0t-a-username <153406659+n0t-a-username@users.noreply.github.com>
2024-03-30 14:06:28 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import "@material/web/button/outlined-button.js";
+import "@material/web/button/text-button.js";
+import "@material/web/icon/icon.js";
+import "@material/web/iconbutton/icon-button.js";
+import "@material/web/textfield/filled-text-field.js";
+import "@material/web/textfield/outlined-text-field.js";
+import "@material/web/iconbutton/filled-icon-button.js";
+import "@material/web/iconbutton/filled-tonal-icon-button.js";
+import "@material/web/iconbutton/icon-button.js";
+import "@material/web/iconbutton/outlined-icon-button.js";
+
+import { MdFilledTextField } from "@material/web/textfield/filled-text-field.js";
+import { css, html, nothing } from "lit";
